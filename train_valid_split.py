2280c03c2f175e81dd353e6b29e910e1952232a9
aqntks <cjd0030@gmail.com>
2022-01-18 15:58:24 +0900

    update

--- train_valid_split.py
+++ train_valid_split.py
@@ -2,20 +2,20 @@
 
 import os
 import random
-path = r"/home/code1system/workspace/data/gas_all_no_serial/image"
+path = r"C:\Users\home\Desktop\car\jjj"
 
 dir_list = os.listdir(path)
 img_list = []
 
 
-f = open("gas_all_no_serial_train.txt", 'a')
-vf = open("gas_all_no_serial_valid.txt", 'a')
+f = open(r"C:\Users\home\Desktop\car\car_train.txt", 'a')
+vf = open(r"C:\Users\home\Desktop\car\car_valid.txt", 'a')
 
 for ddd in dir_list:
    if ddd.split('.')[1].lower() == "jpg" or ddd.split('.')[1].lower() == "png" or ddd.split('.')[1].lower() == "jpeg":
        img_list.append(ddd)
 
-val_list = random.sample(img_list, 2282)
+val_list = random.sample(img_list, 700)
 
 for val in val_list:
     img_list.remove(val)
